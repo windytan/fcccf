204b8a3bc5367e98252b9340f7985bfe4a13404d
Koura <tomppa.j@hotmail.com>
2013-08-26 14:29:39 +0300

    score added and is shown on the screen

--- public_html/js/layer.js
+++ public_html/js/layer.js
@@ -123,6 +123,7 @@ var levelLayer = {
 	items: [],
 	levelNumber: 0,
 	dropRate: 0,
+	score: 0,
 	props: [],
 	cats: [],
 	deadCats: [],
@@ -270,6 +271,11 @@ var levelLayer = {
 			drawItem(this.itemInHand);
 		}
 		drawBackground("foreground");
+		ctx.fillStyle = "#FFFFFF";
+		ctx.font = "20px Arial";
+		ctx.fillText("Score: ", 520, 630);
+		ctx.fillStyle = "#FF00FF";
+		ctx.fillText(this.score , 600, 631);
 	},
 
   spawnItem: function () {
@@ -295,6 +301,7 @@ var levelLayer = {
 				this.items.splice(i, 1);
 				catAI.updateFood(this.items);
 				playSoundEffect('snd/omnom' + soundEffectVariator(2) + '.ogg');
+				this.score++;
 				return true;
 			}
 		}
